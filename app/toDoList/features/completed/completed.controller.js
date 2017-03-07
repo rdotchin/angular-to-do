54d44b7c6c8e586b47d5202ecea6a78a3913044d
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-07 02:15:22 -0500

    small edits

--- app/toDoList/features/completed/completed.controller.js
+++ app/toDoList/features/completed/completed.controller.js
@@ -2,6 +2,13 @@ angular
     .module('toDoList.completed')
     .controller('CompletedCtrl', CompletedCtrl);
 
-function CompletedCtrl(){
+function CompletedCtrl(todos){
+	var vm = this;
+	vm.todoData = [];//Array to hold todo text and completedBy
+	vm.currentDate = new Date();//current date 
+
+	/*call the $http GET function from todos.service to get the todos data and 
+	pass them into the vm.todoData array*/
+	todos.getTodos(vm.todoData); 
 
 }
